18d82c738bfd3ad3b3f24a0132f2f92b1108bb8d
Erwin Doria <doria.erwin027@gmail.com>
2020-09-11 04:43:00 +0800

    update config

--- config.py
+++ config.py
@@ -5,9 +5,6 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
     os.path.join(basedir, 'appointment.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_ECHO = True
-SECRET_KEY = 'SECRET_LANG'
-JWT_AUTH_URL_RULE = '/login'
-JWT_AUTH_USERNAME_KEY = 'email'
 
 PORT = 5000
 HOST = '127.0.0.1'
