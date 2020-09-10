ddc3d352eacda1a306abaf71480ad796f8ee3445
Erwin Doria <doria.erwin027@gmail.com>
2020-09-11 04:40:40 +0800

    custom response

--- service.py
+++ service.py
@@ -1,7 +1,7 @@
 
 from schema import appointment_schema, patient_schema
 from marshmallow import ValidationError
-from util import isValidTime
+from util import isValidTime, response
 from datetime import datetime
 from repository import Repository
 
@@ -20,15 +20,16 @@ class Service:
             if validTime:
                 return validTime
         except ValidationError as err:
-            return {"errors": err.messages}, 422
+            return response(err.messages, False, 422)
         if self.repo.findAppointmentBetweenDateTime(data) != None:
-            return {"errors": "This time already have a booking"}, 400
-        return {"appointment": appointment_schema.dump(self.repo.create(self.Appointment(data)))}
+            return response("This time already have a booking", False, 400)
+
+        return response(appointment_schema.dump(self.repo.create(self.Appointment(data))))
 
     def update(self, request, id):
         appointment = self.Appointment.query.get(id)
         if appointment is None:
-            return {"errors": "Appointment not found"}, 422
+            return response("Appointment not found", False, 422)
         try:
             data = appointment_schema.load(request.get_json())
             data['patient']['id'] = appointment.patient.id
@@ -37,47 +38,47 @@ class Service:
             if validTime:
                 return validTime
         except ValidationError as err:
-            return {"errors": err.messages}, 422
+            return response(err.messages, False, 422)
 
         appointmentBetweenDateTime = self.repo.findAppointmentBetweenDateTime(
             data)
 
         if appointmentBetweenDateTime != None and appointmentBetweenDateTime.id != id:
-            return {"errors": "This time already have a booking"}, 400
+            return response("This time already have a booking", False, 400)
 
         appointment.update(data)
-        return "Successfully updated"
+        return response("Successfully updated", key="message")
 
     def show(self, request):
         try:
             start = datetime.strptime('{}'.format(
                 request.args.get('startDate')), '%Y-%m-%d')
         except:
-            return {"errors": 'invalid startDate format should be YYYY-mm-dd'}, 422
+            return response("invalid startDate format should be YYYY-mm-dd", False, 422)
 
         try:
             end = datetime.strptime('{}'.format(
                 request.args.get('endDate')), '%Y-%m-%d')
         except:
-            return {"errors": 'invalid endDate format should be YYYY-mm-dd'}, 422
+            return response("invalid endDate format should be YYYY-mm-dd", False, 422)
 
         if start > end:
-            return {"errors": 'invalid endDate'}, 422
+            return response("invalid endDate", False, 422)
         elif start == end:
-            return {"errors": 'startDate and endDate should not be equal'}, 422
+            return response("startDate and endDate should not be equal", False, 422)
         elif start is None or end is None:
-            return {"errors": 'required query parameters startDate and endDate'}, 422
+            return response("required query parameters startDate and endDate", False, 422)
 
         appointments = list(
             map(lambda appointment: appointment.serialize(), self.repo.findBetweenDate(start, end)))
 
-        return {"appointments": appointments}
+        return response(appointments, key="appointments")
 
     def delete(self, id):
         appointment = self.Appointment.query.get(id)
         if appointment is None:
-            return {"errors": "Appointment not found"}, 422
-        if self.repo.delete(self.Comment, appointment.comment.id) and self.repo.delete(self.Appointment, id):
-            return "Successfully deleted"
+            return response("Appointment not found", False, 422)
+        if self.repo.delete(self.Patient, appointment.patient.id) and self.repo.delete(self.Comment, appointment.comment.id) and self.repo.delete(self.Appointment, id):
+            return response("Successfully deleted", key="message")
         else:
-            return {"errors": "Unable to delete appointment please try again later"}, 422
+            return response("Unable to delete appointment please try again later", False, 422)
--- util.py
+++ util.py
@@ -5,6 +5,10 @@ def isBetween(start, date, end):
     return start < date < end
 
 
+def response(data, status=True, code=200, key="appointment"):
+    return {"data": {key if status else "errors": data}}, code
+
+
 def isValidTime(startTime, endTime):
     validStarts = [datetime.strptime('{} 08:59:00'.format(
         startTime.date()), '%Y-%m-%d %H:%M:%S'), datetime.strptime('{} 16:31:00'.format(
@@ -15,12 +19,12 @@ def isValidTime(startTime, endTime):
             startTime.date()), '%Y-%m-%d %H:%M:%S')]
 
     if startTime <= datetime.now():
-        return {"errors": "Invalid start time"}, 400
+        return response("Invalid start time", False, 400)
     elif startTime >= endTime:
-        return {"errors": "Invalid end time"}, 400
+        return response("Invalid end time", False, 400)
     elif startTime.strftime("%a") == 'SUN':
-        return {"errors": "Invalid date should Mon - Sat only"}, 400
+        return response("Invalid date should Mon - Sat only", False, 400)
     elif not isBetween(validStarts[0], startTime, validStarts[1]):
-        return {"errors": "Start time should be 9:00am - 4:30pm"}, 400
+        return response("Start time should be 9:00am - 4:30pm", False, 400)
     elif not isBetween(validEnd[0], endTime, validEnd[1]):
-        return {"errors": "End time should be 9:30am - 5:00pm"}, 400
+        return response("End time should be 9:30am - 5:00pm", False, 400)
