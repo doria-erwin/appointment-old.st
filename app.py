2a093da45e63afed56e1969c55b5513044c16369
Erwin Doria <doria.erwin027@gmail.com>
2020-09-11 03:20:32 +0800

    refactor

--- app.py
+++ app.py
@@ -3,11 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, Text, ForeignKey, String, DateTime, Boolean, update
 from sqlalchemy.orm.exc import NoResultFound
 from gevent.pywsgi import WSGIServer
-from schema import appointment_schema, patient_schema
-from marshmallow import ValidationError
-from datetime import datetime
 from config import HOST, PORT
-from repository import repository
+from service import Service
 from util import isValidTime
 
 app = Flask(__name__)
@@ -22,9 +19,8 @@ class Comment(db.Model):
     appointment_id = Column(Integer, ForeignKey('appointment.id'))
     appointment = db.relationship("Appointment", back_populates="comment")
 
-    def __init__(self, message, appointment):
-        self.message = message
-        self.appointment = appointment
+    def __init__(self, data):
+        self.message = data['message']
 
     def serialize(self):
         return{
@@ -39,7 +35,8 @@ class Patient(db.Model):
     firstName = Column(String(50), nullable=False)
     lastName = Column(String(50), nullable=False)
     middleName = Column(String(50), nullable=False)
-    appointment = db.relationship("Appointment",  back_populates="patient")
+    appointment_id = Column(Integer, ForeignKey('appointment.id'))
+    appointment = db.relationship("Appointment", back_populates="patient")
 
     def __init__(self, data):
         self.firstName = data['firstName']
@@ -62,19 +59,29 @@ class Appointment(db.Model):
     endTime = Column(DateTime, nullable=False)
     comment = db.relationship("Comment", uselist=False,
                               back_populates="appointment", lazy='joined')
-    patient_id = Column(Integer, ForeignKey('patient.id'))
     patient = db.relationship(
-        "Patient", back_populates="appointment", lazy='joined')
+        "Patient", uselist=False, back_populates="appointment", lazy='joined')
 
     def __init__(self, data):
         self.startTime = data['startTime']
         self.endTime = data['endTime']
+        self.patient = Patient(data['patient'])
+        self.comment = Comment(data['comment'])
+
+    def update(self, data):
+        self.startTime = data['startTime']
+        self.endTime = data['endTime']
+        Patient.query.filter(
+            Patient.id == data['patient']['id']).update(data['patient'])
+        Comment.query.filter(
+            Comment.id == data['comment']['id']).update(data['comment'])
+        db.session.commit()
 
     def serialize(self):
         return{
             "id": self.id,
             "startTime": self.startTime,
-            "endTime": self.startTime,
+            "endTime": self.endTime,
             "comment": self.comment.serialize(),
             "patient": self.patient.serialize()
         }
@@ -82,107 +89,27 @@ class Appointment(db.Model):
 
 db.create_all()
 
-repo = repository(db.session, Appointment, Patient, Comment)
+service = Service(db.session, Appointment, Patient, Comment)
 
 
 @app.route('/appointment', methods=['POST'])
 def create():
-    try:
-        data = appointment_schema.load(request.get_json())
-        patient = repo.findPatientByName(data['patient'])
-        validTime = isValidTime(data['startTime'], data['endTime'])
-        if validTime:
-            return validTime
-    except ValidationError as err:
-        return {"errors": err.messages}, 422
-    if patient != None and repo.findAppointmentByDateAndPatient(data, patient) != None:
-        return {"errors": "This patient already have a booking in {}".format(data['startTime'].date())}
-    elif repo.findAppointmentBetweenDateTime(data) != None:
-        return {"errors": "This time already have a booking"}, 400
-    else:
-        if patient == None:
-            patient = repo.create(Patient(data['patient']))
-
-        appointment = Appointment(data)
-        appointment.patient = patient
-        appointment = repo.create(appointment)
-
-        comment = repo.create(Comment(data['comment']['message'], appointment))
-        appointment.comment = comment
-
-        return {"appointment": appointment_schema.dump(appointment)}
+    return service.create(request)
 
 
 @app.route('/appointment/<int:id>', methods=['PUT'])
 def updateAppointment(id):
-    appointment = Appointment.query.get(id)
-    if appointment is None:
-        return {"errors": "Appointment not found"}, 422
-    try:
-        data = appointment_schema.load(request.get_json())
-        validTime = isValidTime(data['startTime'], data['endTime'])
-        if validTime:
-            return validTime
-    except ValidationError as err:
-        return {"errors": err.messages}, 422
-
-    appointmentByDate = repo.findAppointmentByDateAndPatient(
-        data, appointment.patient)
-    appointmentBetweenDateTime = repo.findAppointmentBetweenDateTime(data)
-
-    if appointmentByDate != None and appointmentByDate.id != id:
-        return {"errors": "This patient already have a booking in {}".format(data['startTime'].date())}
-    elif appointmentBetweenDateTime != None and appointmentBetweenDateTime.id != id:
-        return {"errors": "This time already have a booking"}, 400
-
-    repo.updateById(Patient, appointment.patient.id, data['patient'])
-    repo.updateById(Comment, appointment.comment.id, data['comment'])
-    repo.updateById(Appointment, appointment.id, {
-        "startTime": data['startTime'], "endTime": data['endTime']})
-    db.session.commit()
-    db.session.refresh(appointment)
-    return {"appointment": Appointment.query.get(id).serialize()}
+    return service.update(request, id)
 
 
 @app.route('/appointments', methods=['GET'])
 def showAll():
-    try:
-        start = datetime.strptime('{}'.format(
-            request.args.get('startDate')), '%Y-%m-%d')
-    except:
-        return {"errors": 'invalid startDate format should be YYYY-mm-dd'}, 422
-
-    try:
-        end = datetime.strptime('{}'.format(
-            request.args.get('endDate')), '%Y-%m-%d')
-    except:
-        return {"errors": 'invalid endDate format should be YYYY-mm-dd'}, 422
-
-    if start > end:
-        return {"errors": 'invalid endDate'}, 422
-
-    if start == end:
-        return {"errors": 'startDate and endDate should not be equal'}, 422
-
-    if start is None or end is None:
-        return {"errors": 'required query parameters startDate and endDate'}, 422
-
-    appointments = list(
-
-        map(lambda appointment: appointment.serialize(), repo.findBetweenDate(start, end)))
-    return {"appointments": appointments}
+    return service.show(request)
 
 
 @app.route('/appointment/<int:id>', methods=['DELETE'])
 def delete(id):
-    appointment = Appointment.query.get(id)
-    if appointment is None:
-        return {"errors": "Appointment not found"}, 422
-
-    if repo.deleteById(Comment, appointment.comment.id) and repo.deleteById(Appointment, id):
-        return "Successfully deleted"
-    else:
-        return {"errors": "Unable to delete appointment please try again later"}, 422
+    return service.delete(id)
 
 
 if __name__ == "__main__":
