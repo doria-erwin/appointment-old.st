7df1709cc614f020604ae51f0d17d77c87ebfb38
Erwin Doria <doria.erwin027@gmail.com>
2020-09-12 23:38:25 +0800

    remove debugging prints

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
     os.path.join(basedir, 'appointment.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_ECHO = True
+SQLALCHEMY_ECHO = False
 
 PORT = 5000
 HOST = '127.0.0.1'
